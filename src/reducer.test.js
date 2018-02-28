12b924cdb2cbb532ef375ad81a86e595cda0c2c0
dermio <thisdjay@gmail.com>
2018-02-27 23:15:21 -0800

    Added one test for reducer.test.js

---
+++ src/reducer.test.js
@@ -0,0 +1,8 @@
+import reducer from "./reducer";
+import {generateAuralUpdate, restartGame, makeGuess} from "./actions";
+
+describe("reducer", function () {
+  it("should set the initial state when nothing is passed in", function () {
+
+  });
+});
