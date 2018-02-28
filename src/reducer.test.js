a5bae3917fa5cc5d6e41ac5abd9b446017c6bb83
dermio <thisdjay@gmail.com>
2018-02-28 10:31:25 -0800

    Tested for initial state when nothing passed in

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,7 +2,15 @@ import reducer from "./reducer";
 import {generateAuralUpdate, restartGame, makeGuess} from "./actions";
 
 describe("reducer", function () {
-  it("should set the initial state when nothing is passed in", function () {
+  it("Should set the initial state when nothing is passed in", function () {
+    let state = reducer(undefined, {type: "__UNKNOWN"});
+    // console.log(state); to look at the initial state
 
+    expect(state.guesses).toEqual([]);
+    expect(state.feedback).toEqual("Make your guess!");
+    expect(state.auralStatus).toEqual("");
+    expect(state.correctAnswer).toBeGreaterThanOrEqual(1);
+    expect(state.correctAnswer).toBeLessThanOrEqual(100);
   });
+
 });
